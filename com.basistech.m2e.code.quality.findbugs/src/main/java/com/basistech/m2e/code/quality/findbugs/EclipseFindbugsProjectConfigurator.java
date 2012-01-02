a475fdc75b9c1c57b3192944c8c5fe3d3e696a23
Bela Eroess <bela.eroess@gmail.com>
2012-01-02 17:13:58 +0100

    adding m2e findbugs connector for findbugs 2.0.0

---
+++ com.basistech.m2e.code.quality.findbugs/src/main/java/com/basistech/m2e/code/quality/findbugs/EclipseFindbugsProjectConfigurator.java
@@ -0,0 +1,120 @@
+/*******************************************************************************
+ * Copyright 2010 Mohan KR
+ * Copyright 2010 Basis Technology Corp.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package com.basistech.m2e.code.quality.findbugs;
+
+import static com.basistech.m2e.code.quality.findbugs.FindbugsEclipseConstants.MAVEN_PLUGIN_ARTIFACTID;
+import static com.basistech.m2e.code.quality.findbugs.FindbugsEclipseConstants.MAVEN_PLUGIN_GROUPID;
+
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.MojoExecution;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.m2e.core.project.IMavenProjectFacade;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.basistech.m2e.code.quality.shared.AbstractMavenPluginProjectConfigurator;
+import com.basistech.m2e.code.quality.shared.MavenPluginWrapper;
+
+import de.tobject.findbugs.FindbugsPlugin;
+import de.tobject.findbugs.preferences.FindBugsPreferenceInitializer;
+import edu.umd.cs.findbugs.config.UserPreferences;
+
+/**
+ */
+public class EclipseFindbugsProjectConfigurator extends
+		AbstractMavenPluginProjectConfigurator {
+
+	private static final Logger log = LoggerFactory
+			.getLogger("com/basistech/m2e/code/quality/findbugs/EclipseFindbugsProjectConfigurator");
+
+	public EclipseFindbugsProjectConfigurator() {
+		super();
+	}
+
+	@Override
+	protected String getMavenPluginArtifactId() {
+		return MAVEN_PLUGIN_ARTIFACTID;
+	}
+
+	@Override
+	protected String getMavenPluginGroupId() {
+		return MAVEN_PLUGIN_GROUPID;
+	}
+
+	@Override
+	protected String[] getMavenPluginGoal() {
+		return new String[] { "findbugs" };
+	}
+
+	@Override
+	protected void handleProjectConfigurationChange(final MavenSession session,
+			final IMavenProjectFacade mavenProjectFacade,
+			final IProject project, final IProgressMonitor monitor,
+			final MavenPluginWrapper mavenPluginWrapper) throws CoreException {
+		log.debug("entering handleProjectConfigurationChange");
+		final MavenPluginConfigurationTranslator mavenFindbugsConfig = MavenPluginConfigurationTranslator
+				.newInstance(this, session,
+						mavenProjectFacade.getMavenProject(monitor),
+						mavenPluginWrapper, project);
+		UserPreferences prefs;
+		try {
+			prefs = this.buildFindbugsPreferences(project, mavenFindbugsConfig,
+					session, mavenPluginWrapper.getMojoExecution());
+			final EclipseFindbugsConfigManager fbPluginNature = EclipseFindbugsConfigManager
+					.newInstance(project);
+			// Add the builder and nature
+			fbPluginNature.configure(monitor);
+			FindbugsPlugin.saveUserPreferences(project, prefs);
+			FindbugsPlugin.setProjectSettingsEnabled(project, null, true);
+		} catch (final CoreException ex) {
+			log.error(ex.getLocalizedMessage());
+		}
+	}
+
+	@Override
+	protected void unconfigureEclipsePlugin(final IProject project,
+			final IProgressMonitor monitor) throws CoreException {
+		log.debug("entering unconfigureEclipsePlugin");
+		final EclipseFindbugsConfigManager fbPluginNature = EclipseFindbugsConfigManager
+				.newInstance(project);
+		fbPluginNature.deconfigure(monitor);
+
+	}
+
+	private UserPreferences buildFindbugsPreferences(final IProject project,
+			final MavenPluginConfigurationTranslator pluginCfgTranslator,
+			final MavenSession session, final MojoExecution execution)
+			throws CoreException {
+		log.debug("entering buildFindbugsPreferences");
+		final UserPreferences prefs = FindBugsPreferenceInitializer
+				.createDefaultUserPreferences();
+		pluginCfgTranslator.setIncludeFilterFiles(prefs);
+		pluginCfgTranslator.setExcludeFilterFiles(prefs);
+		//pluginCfgTranslator.setBugCatagories(prefs);
+		pluginCfgTranslator.setEffort(prefs);
+		pluginCfgTranslator.setVisitors(prefs);
+		pluginCfgTranslator.setOmitVisitors(prefs);
+		pluginCfgTranslator.setPriority(prefs);
+		pluginCfgTranslator.setThreshold(prefs);
+		
+		FindbugsPlugin.DEBUG = pluginCfgTranslator.debugEnabled();
+		return prefs;
+	}
+
+}
---
+++ com.basistech.m2e.code.quality.findbugs/src/main/java/com/basistech/m2e/code/quality/findbugs/FindbugsEclipseConstants.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright 2010 Mohan KR
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package com.basistech.m2e.code.quality.findbugs;
+
+/**
+ * Constants as it relates to eclipse findbugs plugin
+ */
+public final class FindbugsEclipseConstants {
+
+	public static final String LOG_PREFIX = "M2E-FB";
+	public static final String MAVEN_PLUGIN_GROUPID = "org.codehaus.mojo";
+	public static final String MAVEN_PLUGIN_ARTIFACTID = "findbugs-maven-plugin";
+	public static final String ECLIPSE_FB_PREFS_FILE = ".fbprefs";
+	public static final String ECLIPSE_FB_PREFS_CONFIG_NAME = "findbugs-maven-plugin";
+	public static final String ECLIPSE_FB_NATURE_ID = "edu.umd.cs.findbugs.plugin.eclipse.findbugsNature";
+	public static final String FB_EXCLUDE_FILTER_FILE = ".fbExcludeFilterFile";
+	public static final String FB_INCLUDE_FILTER_FILE = ".fbIncludeFilterFile";
+	public static final String VISITORS = "visitors";
+	public static final String THRESHOLD = "threshold";
+	public static final String OMIT_VISITORS = "omitVisitors";
+	public static final String PRIORITY = "priority";
+	public static final String EFFORT = "effort";
+	public static final String DEBUG = "debug";
+	public static final String BUG_CATEGORIES = "bugCategories";
+	public static final String EXCLUDE_FILTER_FILE = "excludeFilterFile";
+	public static final String INCLUDE_FILTER_FILE = "includeFilterFile";
+
+	private FindbugsEclipseConstants() {
+		// no instantiation.
+	}
+
+}
