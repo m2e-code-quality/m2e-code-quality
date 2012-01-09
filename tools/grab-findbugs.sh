6f500dc887a7aaf0855b26865929037dad3fb9ac
Benson Margulies <bmargulies@gmail.com>
2012-01-09 17:36:17 -0500

    Try harder to get the p2 repo for findbugs right.

--- tools/grab-findbugs.sh
+++ tools/grab-findbugs.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 url=http://findbugs.cs.umd.edu/eclipse/
+repo=file://$PWD/../com.basistech.m2e.code.quality.findbugs/findbugs.p2
 eclipse=/opt/eclipse-indigo/eclipse/Eclipse.app/Contents/MacOS/eclipse
 $eclipse -nosplash -verbose \
  -application org.eclipse.equinox.p2.artifact.repository.mirrorApplication \
  -verbose \
  -source $url \
- -destination file://$PWD/../com.basistech.m2e.code.quality.findbugs/findbugs.p2
+ -destination $repo
+
+java -jar /opt/eclipse-indigo/eclipse/plugins/org.eclipse.equinox.launcher_1.2.0.v20110502.jar \
+   -application org.eclipse.equinox.p2.publisher.FeaturesAndBundlesPublisher \
+   -metadataRepository $repo \
+   -artifactRepository $repo \
+   -source ../com.basistech.m2e.code.quality.findbugs/findbugs.p2 \
+   -configs gtk.linux.x86 \
+   -publishArtifacts
