80ea4c562cfa5fcef49084444f7fd1a61213747b
Arlo O'Keeffe <mailtoarlo@gmail.com>
2014-06-03 23:13:54 +0200

    Add checkstyle test fragment project

---
+++ com.basistech.m2e.code.quality.checkstyle.test/src/main/java/com/basistech/m2e/code/quality/checkstyle/test/SimpleTest.java
@@ -0,0 +1,14 @@
+package com.basistech.m2e.code.quality.checkstyle.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
