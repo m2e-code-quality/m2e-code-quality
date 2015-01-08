71484cc42d1625e500b859c320090b080c82d2bc
Hugo Trippaers <hugo@trippaers.nl>
2015-01-08 10:02:37 +0100

    Update support for using a local findbugs p2 repository

--- tools/grab-findbugs.sh
+++ tools/grab-findbugs.sh
@@ -1,17 +1,45 @@
 #!/bin/sh
+
 url=http://findbugs.cs.umd.edu/eclipse/
-repo=file://$PWD/../com.basistech.m2e.code.quality.findbugs/findbugs.p2
-eclipse=/opt/eclipse-indigo/eclipse/Eclipse.app/Contents/MacOS/eclipse
+repo_path=../com.basistech.m2e.code.quality.findbugs/findbugs.p2
+repo=file://$PWD/${repo_path}
+eclipse=
+
+# On MacOS X try to find the Eclipse application using the system
+if [ -n "${OSTYPE}" ] && [[ ${OSTYPE} == darwin* ]] 
+then
+  eclipse_loc=`osascript -e 'tell application "System Events" to POSIX path of (file of process "Eclipse" as alias)'`
+  eclipse=${eclipse_loc}/Contents/MacOS/eclipse
+fi
+
+# configure a default value
+if [ -z "${eclipse}" ] 
+then
+  eclipse=/opt/eclipse-indigo/eclipse/Eclipse.app/Contents/MacOS/eclipse
+fi
+
+if [ \! -x "${eclipse}" ]
+then
+  echo Unabled to execute eclipse, please configure the path to eclipse
+  exit 1
+fi
+
+if [ \! -d "$repo_path" ] 
+then
+  mkdir -p $repo_path
+fi
+
 $eclipse -nosplash -verbose \
  -application org.eclipse.equinox.p2.artifact.repository.mirrorApplication \
  -verbose \
  -source $url \
  -destination $repo
 
-java -jar /opt/eclipse-indigo/eclipse/plugins/org.eclipse.equinox.launcher_1.2.0.v20110502.jar \
-   -application org.eclipse.equinox.p2.publisher.FeaturesAndBundlesPublisher \
-   -metadataRepository $repo \
-   -artifactRepository $repo \
-   -source ../com.basistech.m2e.code.quality.findbugs/findbugs.p2 \
-   -configs gtk.linux.x86 \
-   -publishArtifacts
+$eclipse -nosplash -verbose \
+ -application org.eclipse.equinox.p2.publisher.FeaturesAndBundlesPublisher \
+ -metadataRepository $repo \
+ -artifactRepository $repo \
+ -source $PWD/${repo_path} \
+ -configs gtk.linux.x86 \
+ -publishArtifacts \
+ -append
