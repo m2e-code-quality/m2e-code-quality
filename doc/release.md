c8461d791d8792525ef32139f9cf102e9ca586ce
Erwin Tratar <erwin.tratar@gebit.de>
2018-11-27 09:45:36 +0100

    Automatic changelog

--- doc/release.md
+++ doc/release.md
@@ -1,7 +1,7 @@
 m2e-code-quality build and release
 ==================================
 
-m2e-code-quality is built with Tycho. 
+m2e-code-quality is built with Tycho.
 
 To make a release:
 
@@ -13,19 +13,21 @@ Tycho has some stupid bug that goes off if you don't start from clean.
 3. The index.html at the top of the gh-pages for the m2e-code-quality has some general explanation,
 maintain it.
 
-4. When you're satified and all changes are pushed to the develop branch, tag the head
+4. Release notes are generated automatically from merged PR and closed issues. If you want some custom
+[release summary](https://github.com/github-changelog-generator/github-changelog-generator#using-the-summary-section-feature),
+create a new GitHub Issue, set the label `release-summary` and add it to a GitHub milestone named after the inteded
+release version (see next section). The description of the issue will then be used as the release summary.
+
+5. When you're satified and all changes are pushed to the develop branch, tag the head
 commit with a 3-digit version number (e.g. 1.1.0). This will automatically build a site,
-publish it to 
+publish it to
 [https://m2e-code-quality.github.io/m2e-code-quality-p2-site/](https://m2e-code-quality.github.io/m2e-code-quality-p2-site/),
 and update the development branch to the next version number. Please note that the produced artifacts
 will always have a 4 digit version number, otherwise the release build would be newer then the last snapshot build
 in OSGi's version ordering. A release build is marked with a -r version qualifier. This will also produce a
 [release](https://github.com/m2e-code-quality/m2e-code-quality/releases) in github.
 
-5. Just in case something failed you can repeat step 4 by moving the tag. A re-spin will completely replace the version 
-in the P2 repository. 
-
-6. Edit the [release](https://github.com/m2e-code-quality/m2e-code-quality/releases) in github and provide some 
-meaningful release notes.
+6. Just in case something failed you can repeat step 5 by moving the tag. A re-spin will completely replace the version
+in the P2 repository.
 
 That's it!
---
+++ tools/prepare_release.sh
@@ -0,0 +1,68 @@
+#!/bin/bash -eu
+
+[ "$#" -ne "2" ] && echo "usage: $0 <new version> <release branch name>" && exit 1
+
+# stop on first error
+set -eu
+
+# configure git
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+
+# setup origin
+git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
+
+# verify same commit as branch
+HEAD=$(git rev-parse --verify HEAD)
+export GITHUB_BRANCH=$(git ls-remote -q --refs | grep $HEAD | awk '{print $2}' | sed -e 's/^refs\/heads\///')
+
+C=$(echo $GITHUB_BRANCH | wc -w)
+if [ "$C" -ne "1" ]; then echo "Tag cannot be resolved to branch name" && exit 1; fi
+
+# make sure no stale stuff before checkout
+git reset --hard
+
+# checkout release branch
+echo "checkout $2"
+git fetch origin > /dev/null 2>&1
+git checkout -B $2 origin/$2
+
+# merge develop into release
+git merge --no-ff -X ours origin/$GITHUB_BRANCH
+
+# set version and add all changed POMs and add them to the merge commit
+echo "changing version to $1"
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$1 -Dtycho.mode=maven
+
+# Install github_changelog_generator
+gem install github_changelog_generator
+
+# Generate and add CHANGELOG.md
+echo "Generating CHANGELOG"
+github_changelog_generator -t ${GITHUB_TOKEN} --since-tag 0.12.0.201101081422
+git add CHANGELOG.md
+
+# commit changes to branch (will be pushed after release)
+git commit -a --amend --message "Release $1 [skip ci]" > /dev/null 2>&1
+
+# Get latest (soon to be previous) release
+previous_release_tag=$(curl -s \
+    https://${GITHUB_TOKEN}@api.github.com/repos/${TRAVIS_REPO_SLUG}/releases/latest | \
+        jq -r .tag_name)
+
+# Create release notes
+github_changelog_generator -t ${GITHUB_TOKEN} --output /tmp/CHANGELOG-$1.md --since-tag ${previous_release_tag}
+export RELEASE_CHANGELOG=$(< /tmp/CHANGELOG-$1.md)
+
+# move tag to release branch
+git tag -a -f $TRAVIS_TAG -m "Release $TRAVIS_TAG"
+
+# checkout original branch
+echo "checkout $GITHUB_BRANCH"
+git checkout -B $GITHUB_BRANCH
+
+# merge back release branch to develop
+git merge $2
+
+# reset options
+set +eu
