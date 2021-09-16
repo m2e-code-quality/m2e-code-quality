50d49321645247a2f458f96252c5dbe02b5924fc
Andreas Dangel <andreas.dangel@pmd-code.org>
2021-09-16 20:38:52 +0200

    Update prepare_release.sh

--- tools/prepare_release.sh
+++ tools/prepare_release.sh
@@ -1,68 +1,69 @@
 #!/bin/bash -eu
 
-[ "$#" -ne "2" ] && echo "usage: $0 <new version> <release branch name>" && exit 1
+if [[ "${GITHUB_REF}" != refs/tags/* ]]; then
+    echo "${GITHUB_REF} is not a tag!"
+    exit 1
+fi
+
+
 
 # stop on first error
 set -eu
 
-# configure git
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
+RELEASE_TAG=${GITHUB_REF##refs/tags/}
+RELEASE_BRANCH=master
 
-# setup origin
-git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
+echo "Preparing release ${RELEASE_TAG} on branch ${RELEASE_BRANCH}"
+
+# configure git
+git config --local user.name "m2e-code-quality-bot"
+git config --local user.email "m2e-code-quality-bot@users.noreply.github.com"
 
 # verify same commit as branch
 HEAD=$(git rev-parse --verify HEAD)
-export GITHUB_BRANCH=$(git ls-remote -q --refs | grep $HEAD | awk '{print $2}' | sed -e 's/^refs\/heads\///')
+GITHUB_BRANCH=$(git ls-remote -q --refs | grep "$HEAD" | awk '{print $2}' | sed -e 's/^refs\/heads\///')
 
-C=$(echo $GITHUB_BRANCH | wc -w)
+C=$(echo "$GITHUB_BRANCH" | wc -w)
 if [ "$C" -ne "1" ]; then echo "Tag cannot be resolved to branch name" && exit 1; fi
+if [ "$GITHUB_BRANCH" != "develop" ]; then echo "Tag is not on branch develop" && exit 1; fi
 
 # make sure no stale stuff before checkout
 git reset --hard
 
 # checkout release branch
-echo "checkout $2"
-git fetch origin > /dev/null 2>&1
-git checkout -B $2 origin/$2
+echo "checkout ${RELEASE_BRANCH}"
+git config --local remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
+git fetch --unshallow origin
+git checkout -B "${RELEASE_BRANCH}" "origin/${RELEASE_BRANCH}"
 
 # merge develop into release
-git merge --no-ff -X theirs origin/$GITHUB_BRANCH
+NEW_VERSION="${RELEASE_TAG}.$(date +%Y%m%d%H%M)-r"
+git merge --no-ff -X theirs --message "Release ${NEW_VERSION} [skip ci]" "origin/${GITHUB_BRANCH}"
 
 # set version and add all changed POMs and add them to the merge commit
-echo "changing version to $1"
-mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$1 -Dtycho.mode=maven
-
-# Install github_changelog_generator
-gem install github_changelog_generator
+echo "changing version to ${NEW_VERSION}"
+./mvnw org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion="${NEW_VERSION}" -Dtycho.mode=maven
 
 # Generate and add CHANGELOG.md
 echo "Generating CHANGELOG"
-github_changelog_generator -t ${GITHUB_TOKEN} --since-tag 0.12.0.201101081422
+bundle exec github_changelog_generator \
+    -t "${GITHUB_TOKEN}" \
+    --since-tag 0.12.0.201101081422 \
+    --no-verbose
 git add CHANGELOG.md
 
 # commit changes to branch (will be pushed after release)
-git commit -a --amend --message "Release $1 [skip ci]" > /dev/null 2>&1
-
-# Get latest (soon to be previous) release
-previous_release_tag=$(curl -s \
-    https://${GITHUB_TOKEN}@api.github.com/repos/${TRAVIS_REPO_SLUG}/releases/latest | \
-        jq -r .tag_name)
-
-# Create release notes
-github_changelog_generator -t ${GITHUB_TOKEN} --no-unreleased --output /tmp/CHANGELOG-$1.md --since-tag ${previous_release_tag}
-export RELEASE_CHANGELOG=$(< /tmp/CHANGELOG-$1.md)
+git commit -a --amend --no-edit
 
 # move tag to release branch
-git tag -a -f $TRAVIS_TAG -m "Release $TRAVIS_TAG"
+git tag -a -f "$RELEASE_TAG" -m "Release $RELEASE_TAG"
 
 # checkout original branch
-echo "checkout $GITHUB_BRANCH"
-git checkout -B $GITHUB_BRANCH
+echo "checkout ${GITHUB_BRANCH}"
+git checkout -B "${GITHUB_BRANCH}" "origin/${GITHUB_BRANCH}"
 
 # merge back release branch to develop
-git merge $2
+git merge "${RELEASE_BRANCH}"
 
 # reset options
 set +eu
